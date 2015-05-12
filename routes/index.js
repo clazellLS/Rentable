38d98efd9b957980c816a6890263b515e02b989f
Cameron Lazell <cameron.lazell@lisasoft.com>
2015-05-12 10:10:33 +1000

    add in address searching

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('map', { title: 'Wonders' });
+  res.render('search', { title: 'Wonders' });
 });
 
 module.exports = router;
